2405080541c428c3b52e0198d11f20d81fcbe860
author333 <jahyemjahyem@gmail.com>
2026-07-10 08:02:01 -0600

    Implement 'givemoney' command for economy system

---
+++ src/commands/Economy/givemoney.js
@@ -0,0 +1,64 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { successEmbed, warningEmbed } from '../../utils/embeds.js';
+import { getEconomyData, setEconomyData } from '../../utils/economy.js';
+import { withErrorHandling } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
+const ALLOWED_USERS = [
+    '1421980002693415024'
+];
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('givemoney')
+        .setDescription('Give money to a user')
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('User to give money to')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option
+                .setName('amount')
+                .setDescription('Amount of money')
+                .setRequired(true)
+                .setMinValue(1)
+        ),
+
+    execute: withErrorHandling(async (interaction, config, client) => {
+        const deferred = await InteractionHelper.safeDefer(interaction);
+        if (!deferred) return;
+
+        if (!ALLOWED_USERS.includes(interaction.user.id)) {
+            return InteractionHelper.safeEditReply(interaction, {
+                embeds: [
+                    warningEmbed(
+                        'No Permission',
+                        'You are not allowed to use this command.'
+                    )
+                ]
+            });
+        }
+
+        const target = interaction.options.getUser('user');
+        const amount = interaction.options.getInteger('amount');
+
+        const guildId = interaction.guildId;
+
+        let userData = await getEconomyData(client, guildId, target.id);
+
+        userData.wallet += amount;
+
+        await setEconomyData(client, guildId, target.id, userData);
+
+        return InteractionHelper.safeEditReply(interaction, {
+            embeds: [
+                successEmbed(
+                    'Money Added',
+                    `Successfully gave **$${amount.toLocaleString()}** to <@${target.id}>.`
+                )
+            ]
+        });
+    }, { command: 'givemoney' })
+};
